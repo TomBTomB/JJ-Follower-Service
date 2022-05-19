504d4a97c83d84eaf2f120137694a0c54a76cfb5
Tomás Bruno <tomas.bruno@ing.austral.edu.ar>
2022-05-19 16:46:00 -0300

    initial commit

---
+++ src/main/java/com/tombtomb/jjfollowerservice/JjFollowerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tombtomb.jjfollowerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjFollowerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JjFollowerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tombtomb/jjfollowerservice/JjFollowerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tombtomb.jjfollowerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JjFollowerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
