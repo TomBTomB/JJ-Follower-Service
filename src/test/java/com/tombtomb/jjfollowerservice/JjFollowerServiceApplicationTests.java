49d24ddca4b0aca05c89657264bb15f7752d7fb9
Tomás Bruno <tomas.bruno@ing.austral.edu.ar>
2022-05-23 10:39:57 -0300

    Workflow to publish image

--- src/test/java/com/tombtomb/jjfollowerservice/JjFollowerServiceApplicationTests.java
+++ src/test/java/com/tombtomb/jjfollowerservice/JjFollowerServiceApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class JjFollowerServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
